83d2c79390c335823f4314115a1803d2684b6de7
kelsey Brown <kelsey.brown@kellogg.northwestern.edu>
2025-02-12 01:16:21 +0000

    create tables with ruby

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+class CreateSalespeople < ActiveRecord::Migration[7.1]
+    def change
+      create_table :salespeople do |t|
+        t.string "first_name"
+        t.string "last_name"
+        t.string "email"
+        t.timestamps
+      end
+    end
+  end
+
+  class CreateStudios < ActiveRecord::Migration[7.1]
+    def change
+      create_table :studios do |t|
+        t.string "name"
+        t.timestamps
+      end
+    end
+  end
+
+  class CreateMovies < ActiveRecord::Migration[7.1]
+    def change
+      create_table :movies do |t|
+        t.string "title"
+        t.INTEGER "year_released"
+        t.string "rated"
+        t.INTEGER "studio_id"
+        t.timestamps
+      end
+    end
+  end
+
+
+  class CreateActors < ActiveRecord::Migration[7.1]
+    def change
+      create_table :actors do |t|
+        t.string "name"
+        t.timestamps
+      end
+    end
+  end
+
+
+  class CreateRoles < ActiveRecord::Migration[7.1]
+    def change
+      create_table :roles do |t|
+        t.INTEGER "movie_id"
+        t.INTEGER "actor_id"
+        string "character_name"
+        t.timestamps
+      end
+    end
+  end
+
